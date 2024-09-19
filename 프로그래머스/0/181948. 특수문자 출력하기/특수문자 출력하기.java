209e1eadc72ffe11699ee93e6d6554b6ed72d08b
악덕 <122339395+Virtue14@users.noreply.github.com>
2024-09-20 03:23:36 +0900

    [level 0] Title: 특수문자 출력하기, Time: 135.11 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        var a = sc.nextLine();
+        a = "!@#$%^&*(\\'\"<>?:;\n";
+        
+        System.out.println(a);
+
+    }
+}
